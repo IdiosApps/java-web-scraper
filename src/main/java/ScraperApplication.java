55ea8e7fffdac9a27c5b4378924cc7fdc9704634
James <IdiosApps@users.noreply.github.com>
2020-09-19 13:33:04 +0100

    Make entry point create a ScraperApplication and handle processing through that

--- src/main/java/ScraperApplication.java
+++ src/main/java/ScraperApplication.java
@@ -1,18 +1,24 @@
 import java.util.List;
 
 public class ScraperApplication {
-    private final static String DEFAULT_SEARCH_URL = "https://jsainsburyplc.github.io/serverside-test/site/" +
+    public final static String DEFAULT_SEARCH_URL = "https://jsainsburyplc.github.io/serverside-test/site/" +
             "www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception {
         String searchUrl = DEFAULT_SEARCH_URL; // todo take command line args
 
-        WebScraper scraper = new WebScraper();
-        List<Item> itemSummaries = scraper.getItemsInPage(searchUrl);
+        ScraperApplication application = new ScraperApplication();
+        String json = application.getJsonForPage(searchUrl);
 
-        if (!itemSummaries.isEmpty()) {
-            String jsonSummary = ItemsSummarizer.getJsonSummary(itemSummaries);
-            System.out.println(jsonSummary);
-        }
+        System.out.println(json);
+    }
+
+    public String getJsonForPage(String url) throws Exception {
+        WebScraper scraper = new WebScraper();
+        List<Item> itemSummaries = scraper.getItemsInPage(url);
+
+        if (itemSummaries.isEmpty())
+            throw new Exception("No items found");
+        return ItemsSummarizer.getJsonSummary(itemSummaries);
     }
 }
