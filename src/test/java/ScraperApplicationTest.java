719115fc851cd8f0e3691995bb15dcdf5cc10728
James <IdiosApps@users.noreply.github.com>
2020-09-19 14:33:50 +0100

    Test commons-cli usage with unknownArgTest and userSpecifiedUrlSearchTest

--- src/test/java/ScraperApplicationTest.java
+++ src/test/java/ScraperApplicationTest.java
@@ -1,13 +1,18 @@
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.apache.commons.cli.UnrecognizedOptionException;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ScraperApplicationTest {
 
     @Test
-    public void defaultSearchTest() throws Exception {
+    public void defaultSearchTest() {
         ScraperApplication scraperApplication = new ScraperApplication();
 
         String json = scraperApplication.getJsonForPage(ScraperApplication.DEFAULT_SEARCH_URL);
@@ -18,15 +23,27 @@ public class ScraperApplicationTest {
     }
 
     @Test
-    public void alternativeSearchTest() throws Exception {
+    public void userSpecifiedUrlSearchTest() throws Exception {
+        // n.b. json will be missing all kcal and descriptions
+        // item links are direct to actual Sainsbury's site with different html format to brief's example html format
         String url = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/shop/gb/groceries/fruit-veg/bananas-grapes.html";
+        String[] args = new String[] {"-u", url};
 
-        ScraperApplication scraperApplication = new ScraperApplication();
-        // json missing all kcal and descriptions, as links direct to actual Sainsbury's site with different html format
-        String json = scraperApplication.getJsonForPage(url);
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
 
-        JsonObject jsonArray = new Gson().fromJson(json, JsonObject.class);
+        ScraperApplication.main(args);
 
-        assertEquals("31.46", jsonArray.get("gross").getAsString());
+        assertTrue(outContent.toString().contains("\"gross\": \"31.46\""));
+        outContent.close();
+    }
+
+    @Test
+    public void unknownArgTest() {
+        String[] args = new String[] {"-z", "false"};
+
+        Exception exception = assertThrows(UnrecognizedOptionException.class, () -> ScraperApplication.main(args));
+
+        assertEquals("Unrecognized option: -z", exception.getMessage());
     }
 }
