c3f7d2815610a6bc053f6fe6331b4cac2d9fbf14
James <IdiosApps@users.noreply.github.com>
2020-09-19 14:30:11 +0100

    Add command-line arg parsing with commons-cli

--- src/main/java/ScraperApplication.java
+++ src/main/java/ScraperApplication.java
@@ -1,24 +1,42 @@
+import org.apache.commons.cli.*;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ScraperApplication {
     public final static String DEFAULT_SEARCH_URL = "https://jsainsburyplc.github.io/serverside-test/site/" +
             "www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
-    public static void main (String[] args) throws Exception {
-        String searchUrl = DEFAULT_SEARCH_URL; // todo take command line args
+    public static void main (String[] args) throws ParseException {
+        Options options = new Options();
 
-        ScraperApplication application = new ScraperApplication();
-        String json = application.getJsonForPage(searchUrl);
+        Option inputUrl = new Option("u", "url", true, "url to Sainbury's page");
+        inputUrl.setRequired(false);
+        options.addOption(inputUrl);
 
-        System.out.println(json);
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+            String url = cmd.getOptionValue("url", DEFAULT_SEARCH_URL);
+            ScraperApplication application = new ScraperApplication();
+            String json = application.getJsonForPage(url);
+            System.out.println(json);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("ScraperApplication", options);
+            throw e;
+        }
     }
 
-    public String getJsonForPage(String url) throws Exception {
+    public String getJsonForPage(String url) throws NoSuchElementException {
         WebScraper scraper = new WebScraper();
         List<Item> itemSummaries = scraper.getItemsInPage(url);
 
         if (itemSummaries.isEmpty())
-            throw new Exception("No items found");
+            throw new NoSuchElementException("No items found");
         return ItemsSummarizer.getJsonSummary(itemSummaries);
     }
 }
